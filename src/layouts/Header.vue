2960d26d8a41f776b1e2ff3e83579781f48d747e
noriko1599 <sminko1599@gmail.com>
2020-11-13 20:45:54 +0700

    Update Header.vue

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -1,12 +1,12 @@
 <template>
-  <q-header class="bg-white text-black column header">
+<q-header class="bg-white text-black column header">
     <div class="row">
-      <div class="text-h4 text-left project-name">uvbox</div>
-      <div class="text-h5 info">
-        <q-icon name="info" size="42px"></q-icon>
-      </div>
+        <div class="text-h4 text-left project-name">Rekcol UV</div>
+        <div class="text-h5 info">
+            <q-icon name="info" size="42px"></q-icon>
+        </div>
     </div>
-  </q-header>
+</q-header>
 </template>
 
 <script>
@@ -15,15 +15,17 @@ export default {};
 
 <style lang="scss" scoped>
 .info {
-  margin: 0px 0px 0px auto;
-  cursor: pointer;
+    margin: 0px 0px 0px auto;
+    cursor: pointer;
 }
+
 .project-name {
-  text-transform: uppercase;
+    // text-transform: uppercase;
 }
+
 .header {
-  min-height: 80px;
-  max-height: 80px;
-  padding: 25px;
+    min-height: 80px;
+    max-height: 80px;
+    padding: 25px;
 }
 </style>
